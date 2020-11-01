ef9b59c486f456f2bb534dbf0fe59415071a696e
Bashir <bma.963@gmail.com>
2020-11-01 05:05:39 +0200

    refactoring code

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
+// the function to build oru app 
+Expanded buildKey(@required int colorhex , @required int soundNumber) {
+  return Expanded(
+    child: FlatButton(
+      color: Color(colorhex),
+      onPressed: () {
+        final player = AudioCache();
+        player.play('note$soundNumber.wav');
+      },
+    ),
+  );
+}
 
 class XylophoneApp extends StatelessWidget {
   @override
@@ -11,84 +23,15 @@ class XylophoneApp extends StatelessWidget {
       body: SafeArea(
           child: Center(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Expanded(
-              child: FlatButton(
-                child: Container(
-                    decoration: const BoxDecoration(color: Color(0xff05445E))),
-                padding: EdgeInsets.all(0),
-                onPressed: () {
-                  final player = AudioCache();
-                  player.play('note1.wav');
-                },
-              ),
-            ),
-            Expanded(
-              child: FlatButton(
-                child: Container(
-                    decoration: const BoxDecoration(color: Color(0xff189AB4))),
-                padding: EdgeInsets.all(0),
-                onPressed: () {
-                  final player = AudioCache();
-                  player.play('note2.wav');
-                },
-              ),
-            ),
-            Expanded(
-              child: FlatButton(
-                child: Container(
-                    decoration: const BoxDecoration(color: Color(0xff75E6DA))),
-                padding: EdgeInsets.all(0),
-                onPressed: () {
-                  final player = AudioCache();
-                  player.play('note3.wav');
-                },
-              ),
-            ),
-            Expanded(
-              child: FlatButton(
-                child: Container(
-                    decoration: const BoxDecoration(color: Color(0xffD4F1FA))),
-                padding: EdgeInsets.all(0),
-                onPressed: () {
-                  final player = AudioCache();
-                  player.play('note4.wav');
-                },
-              ),
-            ),
-            Expanded(
-              child: FlatButton(
-                child: Container(
-                    decoration: const BoxDecoration(color: Color(0xff05445E))),
-                padding: EdgeInsets.all(0),
-                onPressed: () {
-                  final player = AudioCache();
-                  player.play('note5.wav');
-                },
-              ),
-            ),
-            Expanded(
-              child: FlatButton(
-                child: Container(
-                    decoration: const BoxDecoration(color: Color(0xff189AB4))),
-                padding: EdgeInsets.all(0),
-                onPressed: () {
-                  final player = AudioCache();
-                  player.play('note6.wav');
-                },
-              ),
-            ),
-            Expanded(
-              child: FlatButton(
-                child: Container(
-                    decoration: const BoxDecoration(color: Color(0xff75E6DA))),
-                padding: EdgeInsets.all(0),
-                onPressed: () {
-                  final player = AudioCache();
-                  player.play('note7.wav');
-                },
-              ),
-            ),
+            buildKey(0xff05445E,1),
+            buildKey(0xff189AB4,2),
+            buildKey(0xff75E6DA,3),
+            buildKey(0xffD4F1FA,4),
+            buildKey(0xff05445E,5),
+            buildKey(0xff189AB4,6),
+            buildKey(0xff75E6DA,7),
           ],
         ),
       )),
